d92b2b846b08adc1adfff190d225b77bc1507b15
Beeno Tung <aabbcc1241@yahoo.com.hk>
2021-02-21 04:06:01 +0800

    added complex query test case

--- src/parser.spec.ts
+++ src/parser.spec.ts
@@ -116,5 +116,31 @@ describe('parser', () => {
       expect(asts[1].type).to.equals('select')
       expect((asts[1] as Select).columns).to.deep.equals(['username'])
     })
+
+    it('should parse complex query', function () {
+      // source of sql: https://www.sqlshack.com/learn-sql-how-to-write-a-complex-select-query/
+      let asts = parseSql(`
+        SELECT country.country_name_eng,
+               SUM(CASE WHEN call.id IS NOT NULL THEN 1 ELSE 0 END)             AS calls,
+               AVG(ISNULL(DATEDIFF(SECOND, call.start_time, call.end_time), 0)) AS avg_difference
+        FROM country
+               LEFT JOIN city ON city.country_id = country.id
+               LEFT JOIN customer ON city.id = customer.city_id
+               LEFT JOIN call ON call.customer_id = customer.id
+        GROUP BY country.id,
+                 country.country_name_eng
+        HAVING AVG(ISNULL(DATEDIFF(SECOND, call.start_time, call.end_time), 0)) >
+               (SELECT AVG(DATEDIFF(SECOND, call.start_time, call.end_time)) FROM call)
+        ORDER BY calls DESC, country.id ASC;
+      `)
+      expect(asts).to.have.lengthOf(1)
+      let select = asts[0] as Select
+      expect(select.type).to.equals('select')
+      expect(select.columns).to.deep.equals([
+        'country_name_eng',
+        'calls',
+        'avg_difference',
+      ])
+    })
   })
 })
